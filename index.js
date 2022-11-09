28313dc84ebded396a4ec1403dd734d8acf4bc9b
Mario Barceló <mariobarcelosospedra@gmail.com>
2022-11-09 07:48:59 +0100

    Add input focus and focusout color logic to inputs with icon

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ input.forEach((input) => {
 	input.addEventListener("focus", () => {
 		console.log("FOCUSIN");
 		input.classList.remove("empty");
+		if (input.parentElement.classList.contains("icon-group")) {
+			input.parentElement.classList.remove("empty");
+			input.parentElement.classList.add("filling");
+		}
 		input.classList.add("filling");
 	});
 
@@ -11,6 +15,10 @@ input.forEach((input) => {
 		console.log("FOCUSOUT");
 		if (!input.value) {
 			input.classList.remove("filling");
+			if (input.parentElement.classList.contains("icon-group")) {
+				input.parentElement.classList.remove("filling");
+				input.parentElement.classList.add("empty");
+			}
 			input.classList.add("empty");
 		}
 	});
